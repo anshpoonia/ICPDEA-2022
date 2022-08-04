c71d9a4f1f4e41889c02e81ac76c4401c72707da
Ansh Poonia <pooniaansh11@gmail.com>
2022-08-04 17:00:35 +0530

    Registered participants

---
+++ registered/script.js
@@ -0,0 +1,36 @@
+const menuButton = document.querySelector('.menu-button');
+
+menuButton.addEventListener('click', () => {
+    document.querySelector('.navBarLinks').classList.toggle('open');
+    menuButton.classList.toggle('open');
+})
+
+loadData()
+
+async function loadData()
+{
+    const res = await fetch('http://10.7.10.95:2000/api/candidates',
+        {
+            method: 'GET',
+            headers: {
+                "Accept": "application/json"
+            }
+        })
+    const data = await res.json()
+    const listHolder = document.querySelector('#participantsList');
+
+    let content = "<table><tr><th>Name</th><th>Affiliation</th><th>Email</th></tr>"
+
+    data.forEach(value => {
+
+        const name = value.name;
+        const affiliation = value.affiliation;
+        const email = value.email;
+
+        const temp = `<tr><td>${name}</td><td>${affiliation}</td><td>${email}</td></tr>`;
+        content = content + temp;
+    });
+
+    content = content + "</table>"
+    listHolder.innerHTML = content;
+}
